3133a70273e6e518fe64cb69db23d4ee276e8a06
Andrew Curley <andrew.curley@gmail.com>
2013-06-03 21:51:36 -0400

    Initialize Rails application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HydraHead::Application.config.secret_token = 'dfa58d0c87407ebac54909e810a51005b3a96f9770fbf6e6feac7afa5c21bdc087a12db7d557e76699a1a7b3437c89d7b0912a1e9d612ab722d1b302c92d1747'
